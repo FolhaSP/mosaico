d3956011351bf209547d71935c7268fd7c48f4cb
Leo Diegues <leodiegues@pm.me>
2025-02-24 16:11:49 -0300

    refactor: fix fade effect implementation (#63)

--- src/mosaico/effects/fade.py
+++ src/mosaico/effects/fade.py
@@ -1,35 +1,16 @@
-from typing import Annotated, Literal
+from typing import Literal
 
+from moviepy.video import fx as vfx
 from moviepy.video.VideoClip import VideoClip
 from pydantic import BaseModel
-from pydantic.fields import Field
-from pydantic.functional_validators import model_validator
-from typing_extensions import Self
+from pydantic.types import PositiveFloat
 
 
 class BaseFadeEffect(BaseModel):
     """Base class for fade effects."""
 
-    start_fade: Annotated[float, Field(ge=0, le=1.0)]
-    """Starting fade scale (1.0 is original opacity)."""
-
-    end_fade: Annotated[float, Field(ge=0, le=1.0)]
-    """Ending fade scale."""
-
-    def apply(self, clip: VideoClip) -> VideoClip:
-        """
-        Apply fade effect to clip.
-
-        :param clip: The clip to apply the effect to.
-        :return: The clip with the effect applied.
-        """
-
-        def fade(t):
-            """Calculate fade factor at time t."""
-            progress = t / clip.duration
-            return self.start_fade + (self.end_fade - self.start_fade) * progress
-
-        return clip.time_transform(fade)
+    duration: PositiveFloat = 1
+    """Duration of the fade effect, in seconds."""
 
 
 class FadeInEffect(BaseFadeEffect):
@@ -38,17 +19,15 @@ class FadeInEffect(BaseFadeEffect):
     type: Literal["fade_in"] = "fade_in"
     """Effect type. Must be "fade_in"."""
 
-    start_fade: Annotated[float, Field(ge=0, le=1)] = 0
-    """Starting fade from invisible object."""
+    def apply(self, clip: VideoClip) -> VideoClip:
+        """
+        Apply fade-in effect to clip.
 
-    end_fade: Annotated[float, Field(ge=0, le=1)] = 1.0
-    """Ending fade scale."""
-
-    @model_validator(mode="after")
-    def _validate_fade_in(self) -> Self:
-        if self.start_fade >= self.end_fade:
-            raise ValueError("For fade-in, start_fade must be less than end_fade")
-        return self
+        :param clip: The clip to apply the effect to.
+        :return: The clip with the effect applied.
+        """
+        fx = vfx.FadeIn(self.duration)
+        return fx.apply(clip)  # type: ignore
 
 
 class FadeOutEffect(BaseFadeEffect):
@@ -57,14 +36,12 @@ class FadeOutEffect(BaseFadeEffect):
     type: Literal["fade_out"] = "fade_out"
     """Effect type. Must be "fade_out"."""
 
-    start_fade: Annotated[float, Field(ge=0, le=1)] = 1.0
-    """Starting fade scale (1.5 times the original size)."""
+    def apply(self, clip: VideoClip) -> VideoClip:
+        """
+        Apply fade-out effect to clip.
 
-    end_fade: Annotated[float, Field(ge=0, le=1)] = 0
-    """Ending fade scale."""
-
-    @model_validator(mode="after")
-    def _validate_fade_out(self) -> Self:
-        if self.start_fade <= self.end_fade:
-            raise ValueError("For fade-out, start_fade must be greater than end_fade")
-        return self
+        :param clip: The clip to apply the effect to.
+        :return: The clip with the effect applied.
+        """
+        fx = vfx.FadeOut(self.duration)
+        return fx.apply(clip)  # type: ignore
