7e095c624671e6c924b9d61e192e25769356983a
leodiegues <leodiegues@pm.me>
2025-03-20 20:42:40 -0300

    test: update OpenAISpeechSynthesizer tests to include api_key

--- tests/speech_synthesizers/test_openai.py
+++ tests/speech_synthesizers/test_openai.py
@@ -8,11 +8,11 @@ def test_pass_instructions_raises_on_non_4o_model() -> None:
     with pytest.raises(
         ValidationError, match="`instructions` cannot be set when model is not from the GPT-4o family or higher."
     ):
-        _ = OpenAISpeechSynthesizer(model="tts-1", instructions="Test")
+        _ = OpenAISpeechSynthesizer(api_key="test", model="tts-1", instructions="Test")
 
 
 def test_pass_instructions_as_synthesize_kwargs_raises_on_non_4o_model() -> None:
-    synthesizer = OpenAISpeechSynthesizer(model="tts-1")
+    synthesizer = OpenAISpeechSynthesizer(api_key="test", model="tts-1")
 
     with pytest.raises(
         ValueError, match="`instructions` cannot be set when model is not from the GPT-4o family or higher."
